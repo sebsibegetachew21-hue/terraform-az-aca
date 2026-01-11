6b01a4dff8032834d819839fabc5c2eb0b146d7f
Sebsibe Haile <sebsibe@Sebsibes-MBP.hsd1.il.comcast.net>
2026-01-11 16:18:03 -0600

    tf pipeline setup

--- main.tf
+++ main.tf
@@ -21,11 +21,29 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = false
 }
 
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "${var.name_prefix}-law-${local.suffix}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+# Container Apps Environment (the shared "network + runtime" boundary for your apps)
+resource "azurerm_container_app_environment" "env" {
+  name                = "${var.name_prefix}-env-${local.suffix}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  # Recommended so you can see logs/metrics in Azure
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+}
+
 # A single public Container App ("hello") with ingress enabled
 resource "azurerm_container_app" "hello" {
   name                         = "${var.name_prefix}-hello-${local.suffix}"
   resource_group_name          = azurerm_resource_group.rg.name
-  container_app_environment_id = var.container_app_environment_id
+  container_app_environment_id = azurerm_container_app_environment.env.id
 
   revision_mode = "Single"
 
